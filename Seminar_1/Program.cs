1b74baab1b97cda033d557e9c2f07d01b1979aaf
Alena <radha73@mail.ru>
2023-03-14 16:41:56 +0500

    add Seminar_1

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.git
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine("Введите число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// int max=number1;
+
+// if (number1 < number2)
+// {
+    // max=number2;
+// }
+// else
+// {
+    // max=number1;
+// }
+//    Console.WriteLine("max=" + max);
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Введите первое число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+
+// int max=number1;
+
+// if (number2>max) max=number2; 
+// if (number3>max) max=number3;  
+
+// Console.WriteLine(max);
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number%2==0)
+// {
+//  Console.WriteLine("Да");
+// }
+// else
+// {
+//   Console.WriteLine("Нет");
+// }
+  
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int first=1;
+
+// while(first < number)
+// {
+//  Console.WriteLine(first + 1);
+//   first+=2;
+// }
